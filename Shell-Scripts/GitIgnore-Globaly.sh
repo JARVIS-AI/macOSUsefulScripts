a6504eab139bcd289c0b10de631f74b4a3aafd89
Jarvis Mercer <ams.h4ck3r@gmail.com>
2018-08-17 00:48:21 +0430

    add new shell

---
+++ Shell-Scripts/GitIgnore-Globaly.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global core.excludesfile ~/.gitignore
+
+echo ".DS_Store" >> ~/.gitignore_global
+echo "._.DS_Store" >> ~/.gitignore_global
+echo "**/.DS_Store" >> ~/.gitignore_global
+echo "**/._.DS_Store" >> ~/.gitignore_global
+
